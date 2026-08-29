db0d987ec473c0e903d952150f530afedaeb60b6
Sunny010-design <sunnykhan8777852335@gmail.com>
2026-08-29 16:53:39 +0530

    Configure Prisma database seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,4 @@
+
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
@@ -6,9 +7,11 @@ export default defineConfig({
 
   migrations: {
     path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
   },
 
   datasource: {
     url: env("DATABASE_URL"),
   },
 });
+
